846ff28712e1dd116693ec1155355c4a9c006e04
uppi <helendile@gmail.com>
2016-12-22 09:41:05 +0300

    interim

---
+++ adventofcode/adv_22.py
@@ -0,0 +1,114 @@
+import re
+try:
+    import queue
+except:
+    import Queue as queue
+
+G_R = re.compile("([0-9]+)-y([0-9]+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)")
+#Size  Used  Avail  Use
+
+
+def solve(inp):
+    nodes = [tuple(map(int, x)) for x in G_R.findall(inp)]
+    print(nodes)
+    avail = [(a, x, y) for (x, y, s, u, a, up) in nodes]
+    use = [(u, x, y) for (x, y, s, u, a, up) in nodes]
+
+    pair_count = 0
+
+    for a, x, y in avail:
+        for u, x1, y1 in use:
+            if (x1 != x or y1 != y) and a >= u and u != 0:
+                pair_count += 1
+
+    return pair_count
+
+
+def find_turns(node_mat):
+    """
+    node_mat is [[used, available, tainted]]
+    """
+    max_y = len(node_mat) - 1
+    max_x = len(node_mat[0]) - 1
+    turns_available = []
+    for _y in range(max_y + 1):
+        for _x in range(max_x + 1):
+            u, a, t = node_mat[_y][_x]
+            for ny in range(_y - 1, _y + 2):
+                for nx in range(_x - 1, _x + 2):
+                    if nx != _x and ny != _y:
+                        continue
+                    if nx >= 0 and nx <= max_x and ny >= 0 and ny <= max_y:
+                        nu, na, nt = node_mat[ny][nx]
+                        if nu == 0:
+                            continue
+                        if nu <= a:
+                            #print("We may move {} to {}".format(nu, a))
+                            turns_available.append((nx, ny, _x, _y))
+                        elif nt != 0 and nt <= a:
+                            print("We may move tainted {} to {}".format(nu, a))
+                            turns_available.append((nx, ny, _x, _y))
+    res = []
+    # print(turns_available)
+    for nx, ny, x, y in turns_available:
+        nm = list((list(l) for l in node_mat))
+        u, a, t = nm[y][x]
+        nu, na, nt = nm[ny][nx]
+        if nt == 0:
+            nm[y][x] = (u + nu, a - nu, t)
+            nm[ny][nx] = (0, na + nu, 0)
+        else:
+            nm[y][x] = (u + nt, a - nt, nt)
+            nm[ny][nx] = (nu - nt, na + nt, 0)
+        res.append(tuple(tuple(l) for l in nm))
+
+    return res
+
+
+def print_state(node_mat):
+    for line in node_mat:
+        print(" ".join("{}/{}{}".format(
+            u, a, "*" if t != 0 else "") for (u, a, t) in line))
+
+
+def solve2(inp):
+    nodes = [tuple(map(int, x)) for x in G_R.findall(inp)]
+    max_x = max(x for (x, y, s, u, a, up) in nodes)
+    max_y = max(y for (x, y, s, u, a, up) in nodes)
+    node_mat = []
+    for _ in range(max_y + 1):
+        node_mat.append([(0, 0)] * (max_x + 1))
+
+    for (x, y, s, u, a, up) in nodes:
+        node_mat[y][x] = (u, a, u if y == 0 and x == max_x else 0)
+
+    for line in node_mat:
+        print(" ".join("{}/{}{}".format(
+            u, a, "*" if t != 0 else "") for (u, a, t) in line))
+
+    q = queue.Queue()
+    state = tuple(tuple(l) for l in node_mat)
+    q.put((state, 0))
+    been_there = set()
+    been_there.add(state)
+    prev_turns = 0
+    while not q.empty():
+        state, turns = q.get_nowait()
+        if turns != prev_turns:
+            print(turns)
+            prev_turns = turns
+
+        states = find_turns(state)
+        for s in states:
+            if s[0][0][2] != 0:
+                return turns + 1
+            if s not in been_there:
+                been_there.add(s)
+                #print_state(s)
+                #print()
+                q.put((s, turns + 1))
+
+
+if __name__ == '__main__':
+    with open("data/22.txt") as inp:
+        print(solve2(inp.read()))
---
+++ tests/test_22.py
@@ -0,0 +1,23 @@
+import unittest
+
+import adventofcode.adv_22 as adv
+
+INPUT = """
+Filesystem            Size  Used  Avail  Use%
+/dev/grid/node-x0-y0   10T    8T     2T   80%
+/dev/grid/node-x0-y1   11T    6T     5T   54%
+/dev/grid/node-x0-y2   32T   28T     4T   87%
+/dev/grid/node-x1-y0    9T    7T     2T   77%
+/dev/grid/node-x1-y1    8T    0T     8T    0%
+/dev/grid/node-x1-y2   11T    7T     4T   63%
+/dev/grid/node-x2-y0   10T    6T     4T   60%
+/dev/grid/node-x2-y1    9T    8T     1T   88%
+/dev/grid/node-x2-y2    9T    6T     3T   66%
+"""
+
+
+class Test21(unittest.TestCase):
+    def test_solve(self):
+        self.assertEqual(
+            7,
+            adv.solve2(INPUT))
